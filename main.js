9e1ede9af908d36f6abd8de1856ac752e9a5a470
TredSolei <dinhnhatbao.work@gmail.com>
2024-03-26 16:29:33 +0700

    update effect

---
+++ main.js
@@ -0,0 +1,39 @@
+window.addEventListener("scroll", function () {
+  if (isScrolledToBottom()) {
+    showSpinner();
+
+    setTimeout(function () {
+      window.location.href = "categories.html";
+    }, 5000);
+  }
+});
+
+function isScrolledToBottom() {
+  return window.innerHeight + window.scrollY >= document.body.offsetHeight;
+}
+
+function showSpinner() {
+  var spinnerContainer = document.createElement("div");
+  spinnerContainer.classList.add("spinner-container");
+
+  var spinner = document.createElement("div");
+  spinner.classList.add("spinner");
+
+  var text = document.createElement("div");
+  text.innerText = "ĐANG CHUẨN BỊ ĐỒ LẶN CHO BẠN NÈ, ĐỢI TÍ NHÉ!";
+  text.classList.add("spinner-text");
+
+  spinnerContainer.appendChild(spinner);
+  spinnerContainer.appendChild(text);
+  document.body.appendChild(spinnerContainer);
+}
+
+function navigateToHome() {
+  window.location.href = "home.html";
+  return false;
+}
+
+function navigateToSignin() {
+  window.location.href = "signin.html";
+  return false;
+}
